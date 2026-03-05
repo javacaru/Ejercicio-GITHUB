be12584b592a26cf39e212db20c1c65aa8091018
javacaru <janna.castaneda@udea.edu.co>
2026-03-04 20:40:26 -0500

    Añado clases  medico, enfermera y  paciente

--- EJERCICIOHOSPITAL.py
+++ EJERCICIOHOSPITAL.py
@@ -9,3 +9,38 @@ class PersonalSalud:
     def __str__(self):
         return f"{self.nombre} | Cédula: {self.cedula}"
     
+# medico, enfermera y paciente heredan de personal salud       
+
+class Medico(PersonalSalud):
+    def __init__(self, nombre, cedula, genero, servicio, turno, especialidad):
+        super().__init__(nombre, cedula, genero, servicio)
+        self.turno = turno.upper()
+        self.especialidad = especialidad.upper()
+        self.pacientes = []
+
+    def asignar_paciente(self, paciente):
+        self.pacientes.append(paciente)
+        paciente.medico_asignado = self
+
+    def __str__(self):
+        return f"MÉDICO: {super().__str__()} | Especialidad: {self.especialidad}"
+    
+class Enfermera(PersonalSalud):
+    def __init__(self, nombre, cedula, genero, servicio, turno, rango):
+        super().__init__(nombre, cedula, genero, servicio)
+        self.turno = turno.upper()
+        self.rango = rango.upper()
+
+    def __str__(self):
+        return f"ENFERMERA: {super().__str__()} | Rango: {self.rango}"
+
+class Paciente(PersonalSalud):
+    def __init__(self, nombre, cedula, genero, servicio):
+        super().__init__(nombre, cedula, genero, servicio)
+        self.medico_asignado = None
+
+    def __str__(self):
+        if self.medico_asignado:
+            return f"PACIENTE: {super().__str__()} | Médico: {self.medico_asignado.nombre}"
+        else:
+            return f"PACIENTE: {super().__str__()} | Sin médico asignado"
